cb409f9dc6f591ae5b8af85fc73fd4aa138b4e4f
namnam99 <onep5286@gmail.com>
2023-03-14 15:22:32 +0900

    save

--- 0x01/time.c
+++ 0x01/time.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 int func1(int N);
+int func2(int arr[], int N);
 
 int main() {
-    int N;
-    scanf("%d", &N);
-    printf("%d\n", func1(N));
+    // int N;
+    // scanf("%d", &N);
+    // printf("%d\n", func1(N));   
+    return 0;
 }
 
 int func1(int N) {
@@ -14,4 +16,13 @@ int func1(int N) {
         if (i%3 == 0 || i%5 == 0) sum += i;
     }
     return sum;
+}
+
+int func2(int arr[], int N) {
+    for(int i=0; i<N; i++) {
+        for(int j=i+1; j<N; j++) {
+            if(arr[i] + arr[j] == 100) return 1;
+        }
+    }
+    return 0;
 }
