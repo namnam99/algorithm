e71e0be929708b56fb301e41d7961e52adb940f8
namnam99 <onep5286@gmail.com>
2023-03-16 14:29:40 +0900

    save

---
+++ 0x03_array/insert_erase.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void insert(int idx, int num, int arr[], int& len);
+void erase(int idx, int arr[]; int& len);
+
+int main(){
+    int arr[10] = {10, 50, 40, 30, 70, 20};
+    int len = 6;
+    insert(3, 60, arr, len);
+    erase(4, arr, len);
+}
+
+void insert(int idx, int num, int arr[], int& len){
+
+}
+
+void erase(int idx, int arr[]; int& len){
+    
+}
